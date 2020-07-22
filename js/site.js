5f7ce4cb2e6cbf3839c1703cd95b6fae459a1c19
Tadd Mencer <taddmencer01@gmail.com>
2020-07-22 17:34:35 -0400

    Mobile Menu update

--- js/site.js
+++ js/site.js
@@ -1,8 +1,10 @@
 function myFunction() {
     var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+    if (x.style.height === "0px") {
+      // x.style.display = "none";
+      x.style.height = "0px"
     } else {
-      x.style.display = "block";
+      // x.style.display = "block";
+      x.style.height = "315px"
     }
   }
